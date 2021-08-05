2b2fb7479c0489d9a18a218454ce0a309113fbfa
jithin varghese <jithin6manjapra@gmail.com>
2021-08-05 14:24:30 +1200

    Starting the project

---
+++ src/Pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Header from '../Components/Header/Header';
+import Banner from '../Components/Banner/Banner';
+
+import Cards from '../Components/Cards/Cards'
+import Footer from '../Components/Footer/Footer';
+
+function Home() {
+  return (
+    <div className="homeParentDiv">
+     <Header/>
+     <Banner/>
+     <Cards/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default Home;
+ 
